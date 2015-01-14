0fac754dc6dad9d3eec8ef3abd66bb4d6f0ece25
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2015-01-14 01:20:38 +0100

    Finished SQLBuilder and ResultSetDecoder

--- src/main/java/at/hackenberger/exporter/utils/ResultSetDecoder.java
+++ src/main/java/at/hackenberger/exporter/utils/ResultSetDecoder.java
@@ -1,21 +1,43 @@
 package at.hackenberger.exporter.utils;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ResultSetDecoder {
 
 	private ResultSet resultSet;
 
-	public ResultSet ResultSetDecoder(ResultSet resultSet) {
-		return null;
+	public ResultSetDecoder(ResultSet resultSet) {
+		this.resultSet = resultSet;
 	}
 
-	public String decode() {
-		return null;
+	public String[][] decode() throws SQLException {
+		if(resultSet.isClosed())
+			throw new SQLException("Can not decode ResultSet it is already closed");
+		ArrayList<ArrayList<String>> out = new ArrayList<>();
+		while(resultSet.next()) {
+			ArrayList<String> z = new ArrayList<String>();
+			for(int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+				Object o = resultSet.getObject(i);
+				if(o == null)
+					z.add("NULL");
+				else
+					z.add(o.toString());
+			}
+			out.add(z);
+		}
+		String[][] array = new String[out.size()][];
+		for (int i = 0; i < out.size(); i++) {
+		    ArrayList<String> row = out.get(i);
+		    array[i] = row.toArray(new String[row.size()]);
+		}
+		resultSet.close();
+		return array;
 	}
 
 	public void setResultSet(ResultSet resultSet) {
-
+		this.resultSet = resultSet;
 	}
 
 }
--- src/main/java/at/hackenberger/exporter/utils/SQLBuilder.java
+++ src/main/java/at/hackenberger/exporter/utils/SQLBuilder.java
@@ -4,7 +4,7 @@ public class SQLBuilder {
 
 	private String sortField;
 
-	private boolean sortDir;
+	private String sortDir;
 
 	private String where;
 
@@ -12,28 +12,37 @@ public class SQLBuilder {
 
 	private String table;
 
-	public void SQLBuilder(String fields, String table) {
-
+	public SQLBuilder(String fields, String table) {
+		this.fields = fields;
+		this.table = table;
 	}
 
-	public SQLBuilder(String fields, String table, String where, boolean sortDir, String sortField) {
-
+	public SQLBuilder(String fields, String table, String where, String sortDir, String sortField) {
+		this(fields,table);
+		this.where = where;
+		this.sortDir = sortDir;
+		this.sortField = sortField;
 	}
 
 	public String generateSQL() {
-		return null;
+		String out = "SELECT "+fields+" FROM " + table;
+		if(where != null)
+			out += " WHERE "+where;
+		if(sortField != null)
+			out += " ORDER BY "+sortField + " " + sortDir;
+		return out;
 	}
 
 	public void setSortField(String sortField) {
-
+		this.sortField = sortField;
 	}
 
 	public void setSortDir(String sortDir) {
-
+		this.sortDir = sortDir;
 	}
 
 	public void setWhere(String where) {
-
+		this.where = where;
 	}
 
 	public void setFields(String fields) {
