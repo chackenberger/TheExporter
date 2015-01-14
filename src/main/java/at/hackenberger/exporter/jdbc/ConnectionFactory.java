008fdc337166bd808f41ff2f286315e4bcd7f087
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2015-01-14 01:21:34 +0100

    Finished Factories

--- src/main/java/at/hackenberger/exporter/jdbc/ConnectionFactory.java
+++ src/main/java/at/hackenberger/exporter/jdbc/ConnectionFactory.java
@@ -1,11 +1,24 @@
 package at.hackenberger.exporter.jdbc;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+
+import at.hackenberger.exporter.jdbc.datasource.DataSourceFactory;
 
 public class ConnectionFactory {
 
-	public static Connection getConnection(String hostname, String username, String password, String dbname) {
-		return null;
+	/**
+	 * 
+	 * @param dbms
+	 * @param hostname
+	 * @param username
+	 * @param password
+	 * @param dbname
+	 * @return
+	 * @throws SQLException when there is an error accessing the database
+	 */
+	public static Connection getConnection(String dbms, String hostname, String username, String password, String dbname) throws SQLException {
+		return DataSourceFactory.getDataSource(dbms, hostname, dbname, username, password).getConnection();
 	}
 
 }
--- src/main/java/at/hackenberger/exporter/jdbc/datasource/DataSourceFactory.java
+++ src/main/java/at/hackenberger/exporter/jdbc/datasource/DataSourceFactory.java
@@ -2,10 +2,23 @@ package at.hackenberger.exporter.jdbc.datasource;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class DataSourceFactory {
 
-	public static DataSource getDataSource(String dbms, String url) {
-		return null;
+	public static DataSource getDataSource(String dbms, String hostname, String dbname, String username, String password) {
+		BasicDataSource ds = new BasicDataSource();
+		
+		if(dbms.equalsIgnoreCase("mysql")) {
+			ds.setDriverClassName("com.mysql.jdbc.Driver");
+			ds.setUrl("jdbc:mysql://"+hostname+":3306/"+dbname);
+			ds.setUsername(username);
+			ds.setPassword(password);
+		}else {
+			return null;
+		}
+		
+		return ds;
 	}
 
 }
